6b9ee9fcf92fb910d7ac20f24059ba198a8c2170
Dmitriy <Dmitriy.Kh.78@yandex.ru>
2023-06-27 18:34:10 +0300

    Z1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в
+// промежутке от N до 1. Выполнить с помощью рекурсии.
+
+Console.Write("Введите число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+
+string NaturalToLow( int count, int numberN)
+{
+    if (count == numberN) return count.ToString()  ;
+    
+       return  count + "," + NaturalToLow(count + 1, numberN); 
+}
+
+Console.WriteLine($"Последовательность: '{NaturalToLow(1, numberN)}' ");
